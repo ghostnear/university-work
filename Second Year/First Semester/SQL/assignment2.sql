6815926dfb147bba02a9e0550e6412272b17f287
GhostNear <ghostnear1841@gmail.com>
2023-11-12 19:44:17 +0200

    DB Work in progress

--- Second Year/First Semester/SQL/assignment2.sql
+++ Second Year/First Semester/SQL/assignment2.sql
@@ -106,3 +106,79 @@ SELECT * FROM Reader;
 DELETE FROM Publisher WHERE NAMING LIKE 'ToDelete%';
 DELETE FROM ReaderToManga WHERE ReaderID = 3;
 DELETE FROM Reader WHERE UserName IN ('Somebody', 'SomebodyElse');
+
+-- a) 2 queries with the union operation; use UNION [ALL] and OR;
+SELECT * FROM Manga WHERE MangaType = 'manga'
+UNION
+SELECT * FROM Manga WHERE MangaType = 'manhwa';
+
+SELECT * FROM Manga WHERE MangaType = 'manga' OR MangaType = 'manhwa';
+
+-- b) 2 queries with the intersection operation; use INTERSECT [ALL] and AND;
+SELECT * FROM Manga WHERE MangaType = 'manga'
+INTERSECT
+SELECT * FROM Manga WHERE PhysicallyOwned = 1;
+
+SELECT * FROM Manga WHERE MangaType = 'manga' AND PhysicallyOwned = 1;
+
+-- c) 2 queries with the difference operation; use EXCEPT [ALL] and NOT;
+SELECT * FROM Manga WHERE MangaType = 'manga'
+EXCEPT
+SELECT * FROM Manga WHERE PhysicallyOwned = 1;
+
+SELECT * FROM Manga WHERE MangaType = 'manga' AND NOT PhysicallyOwned = 1;
+
+-- d) 4 queries with INNER JOIN, LEFT JOIN, RIGHT JOIN, and FULL JOIN (one query per operator); one query will join at least 3 tables, while another one will join at least two many-to-many relationships;
+SELECT * FROM Manga
+INNER JOIN MangaCover ON Manga.MCover = MangaCover.ID
+INNER JOIN MangasToGenres ON Manga.ID = MangasToGenres.MangaID;
+
+SELECT * FROM Manga
+LEFT JOIN MangaCover ON Manga.MCover = MangaCover.ID;
+
+SELECT * FROM Manga
+RIGHT JOIN MangaCover ON Manga.MCover = MangaCover.ID;
+
+SELECT * FROM Manga
+FULL JOIN MangasToGenres ON Manga.ID = MangasToGenres.MangaID
+FULL JOIN MangasToAuthors ON Manga.ID = MangasToAuthors.MangaID
+FULL JOIN MangaTagsToManga ON Manga.ID = MangaTagsToManga.MangaID;
+
+-- e) 2 queries with the IN operator and a subquery in the WHERE clause; in at least one case, the subquery must include a subquery in its own WHERE clause;
+SELECT * FROM Manga WHERE Manga.ID IN (SELECT MangaID FROM MangasToGenres WHERE GenreID = 'Action');
+SELECT * FROM Manga WHERE Manga.ID IN (
+    SELECT MangaID FROM MangasToGenres WHERE GenreID IN (
+        SELECT GenreID FROM MangasToGenres WHERE MangaID = 1
+    )
+)
+
+-- f) 2 queries with the EXISTS operator and a subquery in the WHERE clause;
+SELECT * FROM Manga WHERE EXISTS (SELECT * FROM Manga WHERE MangaType = 'manhwa');
+SELECT * FROM Manga WHERE EXISTS (
+    SELECT * FROM Manga WHERE MangaType = 'manhwa' AND PhysicallyOwned = 1
+)
+
+-- g) 2 queries with a subquery in the FROM clause;
+SELECT * FROM (SELECT * FROM Manga WHERE MangaType = 'manga') AS Manga WHERE Manga.PhysicallyOwned = 1;
+SELECT * FROM (SELECT * FROM Manga WHERE MangaType = 'manga' AND PhysicallyOwned = 1) AS Manga WHERE Manga.TotalChapters > 100;
+
+-- h) 4 queries with the GROUP BY clause, 3 of which also contain the HAVING clause; 2 of the latter will also have a subquery in the HAVING clause; use the aggregation operators: COUNT, SUM, AVG, MIN, MAX;
+SELECT MangaType, COUNT(*) FROM Manga GROUP BY MangaType;
+SELECT MangaType, COUNT(*) FROM Manga GROUP BY MangaType HAVING COUNT(*) > 2;
+SELECT MangaType, COUNT(*) FROM Manga GROUP BY MangaType HAVING COUNT(*) > (SELECT COUNT(*) FROM Manga WHERE MangaType = 'manga');
+SELECT MangaType, COUNT(*) FROM Manga GROUP BY MangaType HAVING COUNT(*) > (SELECT COUNT(*) FROM Manga WHERE MangaType = 'manga' AND PhysicallyOwned = 1);
+
+-- i) 4 queries using ANY and ALL to introduce a subquery in the WHERE clause (2 queries per operator);
+SELECT * FROM Manga WHERE TotalChapters > ANY (SELECT TotalChapters FROM Manga WHERE MangaType = 'manga');
+SELECT * FROM Manga WHERE TotalChapters > ALL (SELECT TotalChapters FROM Manga WHERE MangaType = 'manga');
+
+SELECT * FROM Manga WHERE TotalChapters > ANY (SELECT TotalChapters FROM Manga WHERE MangaType = 'manga' AND PhysicallyOwned = 1);
+SELECT * FROM Manga WHERE TotalChapters > ALL (SELECT TotalChapters FROM Manga WHERE MangaType = 'manga' AND PhysicallyOwned = 1);
+
+-- rewrite 2 of them with aggregation operators, and the other 2 with IN / [NOT] IN.
+SELECT * FROM Manga WHERE TotalChapters > (SELECT MIN(TotalChapters) FROM Manga WHERE MangaType = 'manga');
+SELECT * FROM Manga WHERE TotalChapters > (SELECT MAX(TotalChapters) FROM Manga WHERE MangaType = 'manga');
+
+-- TODO: fix this
+SELECT * FROM Manga WHERE TotalChapters IN (SELECT TotalChapters FROM Manga WHERE MangaType = 'manga' AND PhysicallyOwned = 1);
+SELECT * FROM Manga WHERE TotalChapters NOT IN (SELECT TotalChapters FROM Manga WHERE MangaType = 'manga' AND PhysicallyOwned = 1);

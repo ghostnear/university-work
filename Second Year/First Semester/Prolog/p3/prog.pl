a08071613d324d87930b2e9e2ae28d573ed68dba
GhostNear <ghostnear1841@keemail.me>
2023-11-13 10:30:44 +0200

    Remove intermediary result

--- Second Year/First Semester/Prolog/p3/prog.pl
+++ Second Year/First Semester/Prolog/p3/prog.pl
@@ -6,21 +6,19 @@
 %
 
 % Auxiliary to find a method to write the number as a sum of numbers from the set.
-% subset_sum_one(L - list, R - result list, S - sum, PR - partial result)
-% flow model: (i, o, i, _)
+% subset_sum_one(L - list, R - result list, S - sum)
+% flow model: (i, o, i)
 % subset_sum_one(S, D)  = [] if S = 0 or L = []
 %                       = subset_sum_one(S, T) if S < H
 %                       = H + subset_sum_one(S - H, T) if S >= H
-subset_sum_one(L, R, S) :-
-  subset_sum_one(L, R, S, []).  % Alias
-subset_sum_one([], [], _, _) :- fail.
-subset_sum_one(_, [], 0, _) :- !.
-subset_sum_one([H|T], [H|RT], S, PR):-
+subset_sum_one([], [], _) :- fail.
+subset_sum_one(_, [], 0) :- !.
+subset_sum_one([H|T], [H|RT], S):-
   S >= H,
   S1 is S - H,
-  subset_sum_one(T, RT, S1, [H|PR]).
-subset_sum_one([_|T], R, S, PR):-
-  subset_sum_one(T, R, S, PR).
+  subset_sum_one(T, RT, S1).
+subset_sum_one([_|T], R, S):-
+  subset_sum_one(T, R, S).
 
 % Final predicate.
 % subset_sum(L - list, R - result list, S - sum)
